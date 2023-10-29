89a6e4046cadd400422a6508f84103d7a06aa763
khammad5 <khammadakh@code.edu.az>
2023-10-29 10:19:36 +0400

    Add project files.

---
+++ 29.10.2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _29._10._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
